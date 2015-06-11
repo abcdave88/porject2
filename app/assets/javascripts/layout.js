cdea211b4bbd71a1f29cd482de6fcd88e79b9abf
David James <d.james88@btinternet.com>
2015-06-11 10:37:23 +0100

    drop box value captured

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -3,24 +3,26 @@
 //     '<input class="toggle" type="checkbox" data-id="'+ data.id +'" '+ (data.done == 't' ? 'checked="checked"' : "") + '>'+
 //     '<label>'+ data.item +'</label>'+
 //     '<button class="destroy" data-id="'+ data.id +'"></button>'+
-//     '</li>').prependTo("#todo-list")
+//     '</li>').prependTo(".results")
 // }; what the fuck?!
 
 
-// function findInfo(){
-//   console.log('get me those data biatches!');
-//   $.ajax({
-//     type: 'GET', 
-//     url: '/search/find' - I dont know 
-//     dataType: 'json',
-//     data: {item: itemData}
-//   })done(function(data){
-//     $.each(data, function(index, item){
-//       appendSearchItem(item);
-//       console.log('FUCK YOU!');
-//     })
-//   })
-// };
+function findInfo(){
+  var query = this.options[this.selectedIndex].innerHTML; 
+  //console.log(query);
+  
+  // $.ajax({
+  //   type: 'GET', 
+  //   url: '/search/find' 
+  //   dataType: 'json',
+  //   data: {item: itemData}
+  // })done(function(data){
+  //   $.each(data, function(index, item){
+  //     appendSearchItem(item);
+  //     console.log('FUCK YOU!');
+  //   })
+  // })
+};
 
 
 $(document).ready(function(){
